fd09978c42b36402b7761bb37f71e877b12ceea2
晴日 宮沢 <k024g0064@g.neec.ac.jp>
2024-10-16 12:40:42 +0900

    commit

--- PM1_Git_1.cpp
+++ PM1_Git_1.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
 
-    std::cout << "PullRecquest branch change\n";
+    std::cout << "Conflict changed \n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
