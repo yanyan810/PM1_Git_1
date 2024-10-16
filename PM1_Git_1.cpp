55773dcfb771a3831319c3e70045845d32d165d4
晴日 宮沢 <k024g0064@g.neec.ac.jp>
2024-10-16 12:12:55 +0900

    プルリクコミット

--- PM1_Git_1.cpp
+++ PM1_Git_1.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello VisualStudio Git!\n";
 
-    std::cout << "Hello Branch!\n";
+    std::cout << "PullRecquest branch change\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
